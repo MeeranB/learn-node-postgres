1c9c7e41b827345a943512050d1817c94f3b0e65
Meeran B <v9@live.co.uk>
2021-01-14 16:46:31 +0000

    workshop completion

--- workshop/handlers.js
+++ workshop/handlers.js
@@ -1,6 +1,12 @@
+const db = require("./database/connection");
+
 function home(request, response) {
-  response.writeHead(200, { "content-type": "text/html" });
-  response.end(`<h1>Hello world</h1>`);
+  db.query("SELECT username FROM users").then(result => {
+    const resultArr = result.rows;
+    const users = resultArr.map(row => `<li>${row.username}</li>`);
+    response.writeHead(200, { "content-type": "text/html" });
+    response.end(`<ul>${users.join("")}</ul>`);
+  });
 }
 
 function newUser(request, response) {
@@ -24,9 +30,20 @@ function createUser(request, response) {
   request.on("end", () => {
     const searchParams = new URLSearchParams(body);
     const data = Object.fromEntries(searchParams);
-    console.log(data); // e.g. { username: "oli", ... }
-    response.writeHead(200, { "content-type": "text/html" });
-    response.end(`<h1>Thanks for submitting</h1>`);
+    const values = [data.username, data.age, data.location];
+    db.query(
+      "INSERT INTO users(username, age, location) VALUES($1, $2, $3)",
+      values
+    )
+      .then(() => {
+        response.writeHead(302, { location: "/" });
+        response.end();
+      })
+      .catch(error => {
+        console.log(error);
+        response.writeHead(500, { "content-type": "text/html" });
+        response.end(`<h1>Something went wrong saving your data</h1>`);
+      });
   });
 }
 
